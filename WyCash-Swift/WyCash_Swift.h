2ed90d2ee4f14e5262f49249d3794225b6ed9ac9
Miguel <gannasong@gmail.com>
2021-03-08 07:12:11 +0800

    init

---
+++ WyCash-Swift/WyCash_Swift.h
@@ -0,0 +1,18 @@
+//
+//  WyCash_Swift.h
+//  WyCash-Swift
+//
+//  Created by SUNG HAO LIN on 2021/3/8.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WyCash_Swift.
+FOUNDATION_EXPORT double WyCash_SwiftVersionNumber;
+
+//! Project version string for WyCash_Swift.
+FOUNDATION_EXPORT const unsigned char WyCash_SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WyCash_Swift/PublicHeader.h>
+
+
