22d800ccd1f6b6e262bc715caf417f4542995dc5
Miguel <gannasong@gmail.com>
2021-03-08 08:05:35 +0800

    Test: basic multiplier logic

--- WyCash-SwiftTests/WyCashTests.swift
+++ WyCash-SwiftTests/WyCashTests.swift
@@ -7,6 +7,23 @@
 
 import XCTest
 
+class Dollar {
+  var amount: Int
+
+  init(_ amount: Int) {
+    self.amount = amount
+  }
+
+  func times(_ multiplier: Int) {
+    amount *= multiplier
+  }
+}
+
 class WyCashTests: XCTestCase {
 
+  func test_Multiplication() {
+    let five = Dollar(5)
+    five.times(2)
+    XCTAssertEqual(10, five.amount)
+  }
 }
